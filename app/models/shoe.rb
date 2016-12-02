547c8eb5ca0be08af9d38aef28e82a2603694158
jeffrey-meesters <jjeffreymeesters@gmail.com>
2016-12-02 17:29:56 +0100

    Add footer Delete a lot of cart stuff

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -4,6 +4,5 @@ class Shoe < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
-  validates :image_tag, presence: true
 
 end
