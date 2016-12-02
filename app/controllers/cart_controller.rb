d0127fddc23a96088bc06b039ee897ae6ae20d02
jeffrey-meesters <jjeffreymeesters@gmail.com>
2016-12-02 14:09:36 +0100

    Add Cartcontroller options and Cart helper and simple cart index view

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,2 +1,32 @@
 class CartController < ApplicationController
+  # before_action :authenticate_user!, except: [:index]
+  include CartHelper
+
+
+  def index
+    id = params[:id]
+  # if there is a cart, show the cart with info, otherwise show an empy cart
+    if session[:cart]
+      @cart = session[:cart]
+    else
+      @cart = {}
+    end
+  end
+
+  def add
+    add_item
+    render "cart/index"
+  end
+
+  def subtract
+    subtract_item
+    render "cart/index"
+  end
+
+
+  def clearCart
+    session[:cart] = nil
+    redirect_to :action => :index
+  end
+
 end
