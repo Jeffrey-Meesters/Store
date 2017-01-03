c87b9cde16bf508a1f5c78415d36758fd01018c9
jeffrey-meesters <jjeffreymeesters@gmail.com>
2017-01-03 13:58:16 +0100

    Update views to render cart and items

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -2,11 +2,11 @@ class ShoesController < ApplicationController
 
   def index
     @shoes = Shoe.all
-    @order_item = current_order.order_items.new
   end
 
   def show
     @shoe = Shoe.find(params[:id])
+    @order_item = current_order.order_items.new
   end
 
   def new
