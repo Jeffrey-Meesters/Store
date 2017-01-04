eff644fdffa0596fe5326b19ca07f2c45deecaf8
jeffrey-meesters <jjeffreymeesters@gmail.com>
2017-01-04 12:13:13 +0100

    Stripe works

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,7 +5,19 @@ class Order < ApplicationRecord
   before_save :update_subtotal
 
   def subtotal
-  order_items.collect { |oi| oi.valid? ? (oi.quantity * oi.unit_price) : 0 }.sum
+    order_items.collect { |oi| oi.valid? ? (oi.quantity.to_i * oi.unit_price.to_f) : 0 }.sum
+  end
+
+  def shipping
+    order_items.collect { |oi| oi.valid? ? (oi.quantity.to_i * oi.unit_price * 0.05).round(2) : 0 }.sum
+  end
+
+  def tax
+    (subtotal * 0.21).round(2)
+  end
+
+  def total
+    (subtotal + shipping + tax).round(2)
   end
 
 private
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
+  :publishable_key => ENV['sk_test_I1ouewBuDyj7YpwYxkIkLx1K'],
+  :secret_key      => ENV['pk_test_Jm1P1lVhQQIqIt7G5yuPgJQX']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
